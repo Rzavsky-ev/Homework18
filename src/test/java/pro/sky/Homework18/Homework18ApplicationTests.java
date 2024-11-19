e41f4357fb58404eff8af39cb002921e71ad099a
Rzavsky-ev <rzavsky.ev@gmail.com>
2024-11-19 10:48:57 +0300

    Первый коммит

---
+++ src/test/java/pro/sky/Homework18/Homework18ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
